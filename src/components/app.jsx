917c7e1f0eb8c6e7706148616bc6391a00d6d433
Chad High <cdhqwebdev@gmail.com>
2017-10-13 16:53:10 +0000

    major update to all files

--- src/components/app.jsx
+++ src/components/app.jsx
@@ -1,26 +1,91 @@
 import React, { Component } from 'react';
-import '../app.css';
+import { connect } from 'react-redux';
+import { addReminder, deleteReminder } from '../actions';
+import moment from 'moment';
+
+
 
 
 class App extends Component{
+    constructor(props){
+        super(props);
+        this.state = {
+            text: ''
+        };
+    }
+    
+    addReminder(){
+        this.props.addReminder(this.state.text, this.state.dueDate);
+    }
+    
+    deleteReminder(id){
+    this.props.deleteReminder(id);
+    }
+    
+    renderReminders(){
+        const { reminders } = this.props;
+        return(
+            <ul className='list-group col-sm-3'>
+               {
+                   reminders.map(reminder => {
+                       return(
+                       <li key={reminder.id} className='list-group-item'>
+                         <div className='list-item'>
+                          <div>{ reminder.text }</div>
+                          <div><em>{moment(new Date(reminder.dueDate )).fromNow()}</em></div>
+                         </div>
+                         <div className='list-item delete-button' onClick={() => this.deleteReminder(reminder.id)}>
+                            &#x2715;
+                         </div>
+                       </li>
+                       );
+                   })
+               }
+            
+            </ul>
+            );
+    }
+    
     render(){
       return(
           <div className='App'>
             <div className='title'>
                Reminder Pro
             </div>
-            <div className='form-inline'>
+            <div className='form-inline reminder-form'>
               <div className='form-group'>
-                <input
-                 className='form-control'
-                 placeholder='I have to ...'
+                <input 
+                  className='form-control' 
+                  placeholder='I have to ...' 
+                  onChange={event => this.setState({text: event.target.value})} 
+                />
+                
+                <input 
+                className='from-control'
+                type='datetime-local'
+                onChange={event => this.setState({dueDate: event.target.value})}
                 />
               </div>
-              <button type='button' className='btn btn-success'>Add Reminder!!</button>
+              <button 
+                 type='button' 
+                 className='btn btn-success'
+                 onClick={() => this.addReminder()}>
+                 Add Reminder!!
+              </button>
+             
             </div>
+             { this.renderReminders() }
           </div>
       );
     }          
 } 
 
-export default App;
+function mapStateToProps(state){
+    return{
+        reminders: state
+    };
+}
+
+
+
+export default connect(mapStateToProps, { addReminder, deleteReminder }) (App);
